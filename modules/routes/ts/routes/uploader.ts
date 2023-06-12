df0b69a93a5be0f5fdaf1ab7a33a296028428b44
=Julio Rodriguez <jircdev@gmail.com>
2023-06-12 12:16:09 -0300

    chore: ADjust in upload parameters

--- modules/routes/ts/routes/uploader.ts
+++ modules/routes/ts/routes/uploader.ts
@@ -1,22 +1,34 @@
-import {join} from 'path';
-import {FilestoreFile} from '../bucket/file';
-import {generateCustomName} from './utils/generate-name';
-import {getExtension} from './utils/get-extension';
-
+import { join } from 'path';
+import { FilestoreFile } from '../bucket/file';
+import { generateCustomName } from './utils/generate-name';
+import { getExtension } from './utils/get-extension';
+/**
+ *
+ * @param req
+ * @param res
+ * @returns
+ */
 export const uploader = async function (req, res) {
 	if (!req.files.length) {
-		return res.status(400).send({status: false, error: 'No file was uploaded'});
+		return res.status(400).send({ status: false, error: 'No file was uploaded' });
 	}
 
-	const {project, container, topic} = req.body;
+	/**
+	 * project,
+	 * type: document, audio,
+	 * container: folder name to organize
+	 * userId
+	 */
+
+	const { project, type, container, userId } = req.body;
 	try {
 		const promises = [];
 		Object.values(req.files).forEach(file => {
 			// @ts-ignore
-			const {path, originalname, mimetype} = file;
+			const { path, originalname, mimetype } = file;
 
 			const name = `${generateCustomName(originalname)}${getExtension(mimetype)}`;
-			const dest = join(project, container, topic, name);
+			const dest = join(project, type, userId, container, name);
 
 			const fileManager = new FilestoreFile();
 			promises.push(fileManager.upload(path, dest));
