56d31046e870acfcf437859fdaff2f4a1dbe98fe
ftovar8 <felix.godo@gmail.com>
2023-07-02 17:52:21 -0300

    feat: embedding clean code

--- modules/embeddings/ts/documents/index.ts
+++ modules/embeddings/ts/documents/index.ts
@@ -6,33 +6,31 @@ import { TextLoader } from 'langchain/document_loaders/fs/text';
 import { downloadFiles } from './download';
 
 export /*bundle*/ class DocsManager {
-    #splitter = new CharacterTextSplitter();
+	#splitter = new CharacterTextSplitter();
 
-    #path: string;
-    get path() {
-        return this.#path;
-    }
-    #items;
-    get items() {
-        return this.#items;
-    }
+	#path: string;
+	get path() {
+		return this.#path;
+	}
+	#items;
+	get items() {
+		return this.#items;
+	}
 
-    async prepare(path: string, metadata = {}) {
-        if (!path) return { status: false, error: 'path not defined' };
+	async prepare(path: string, metadata = {}) {
+		if (!path) return { status: false, error: 'path not defined' };
 
-        this.#path = path.replace(/\\/g, '/');
-        const tempPath = await downloadFiles(this.#path);
+		this.#path = path.replace(/\\/g, '/');
+		const tempPath = await downloadFiles(this.#path);
 
-        console.log('Docs api - docs prepare', tempPath);
-        const loader = new DirectoryLoader(tempPath, {
-            '.docx': path => new DocxLoader(path),
-            '.pdf': path => new PDFLoader(path),
-            '.txt': path => new TextLoader(path),
-        });
-        this.#items = await loader.loadAndSplit(this.#splitter);
-        this.#items.forEach(item => (item.metadata = Object.assign(item.metadata, metadata)));
+		const loader = new DirectoryLoader(tempPath, {
+			'.docx': path => new DocxLoader(path),
+			'.pdf': path => new PDFLoader(path),
+			'.txt': path => new TextLoader(path),
+		});
+		this.#items = await loader.loadAndSplit(this.#splitter);
+		this.#items.forEach(item => (item.metadata = Object.assign(item.metadata, metadata)));
 
-        console.log('docs api - prepare items', this.#items);
-        return { status: true };
-    }
+		return { status: true };
+	}
 }
