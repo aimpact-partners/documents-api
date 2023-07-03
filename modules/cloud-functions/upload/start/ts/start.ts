fab3e227fa7244bcb59f9dadef97d899db71f8f9
ftovar8 <felix.godo@gmail.com>
2023-07-03 14:04:40 -0300

    feat: set headers on upload

--- modules/cloud-functions/upload/start/ts/start.ts
+++ modules/cloud-functions/upload/start/ts/start.ts
@@ -3,5 +3,16 @@ import { uploader } from '@aimpact/documents-api/routes';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+const setHeaders = res => {
+	res.set('Access-Control-Allow-Origin', '*');
+	res.set('Access-Control-Allow-Methods', 'POST');
+	res.set('Access-Control-Allow-Headers', 'Content-Type');
+	res.set('Access-Control-Max-Age', '3600');
+};
+const run = (req, res) => {
+	setHeaders(res);
+	uploader(req, res);
+};
+
 const { FUNCTION_REGION, CLOUD_FUNCTION } = process.env;
-FUNCTION_REGION && CLOUD_FUNCTION === 'upload' && functions.http('upload', uploader);
+FUNCTION_REGION && CLOUD_FUNCTION === 'upload' && functions.http('upload', run);
