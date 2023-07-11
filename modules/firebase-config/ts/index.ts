2584d811a32cb11c54340b3c8e64565718fc022c
ftovar8 <felix.godo@gmail.com>
2023-07-11 14:38:24 -0300

    firebase-config module added

---
+++ modules/firebase-config/ts/index.ts
@@ -0,0 +1,12 @@
+export /*bundle*/ function getFirebaseConfig() {
+    const config = {
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTHDOMAIN,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID,
+        measurementId: process.env.MEASURAMENTID,
+    };
+    return config;
+}
